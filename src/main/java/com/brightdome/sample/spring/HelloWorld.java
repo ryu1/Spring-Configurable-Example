67a0d6ba7fe71de24a3f8639fef32394beaf7054
Ryuichi.Ishitsuka <jeremy.hillary.boob@gmail.com>
2013-01-11 23:04:53 +0900

    test

--- src/main/java/com/brightdome/sample/spring/HelloWorld.java
+++ src/main/java/com/brightdome/sample/spring/HelloWorld.java
@@ -6,8 +6,10 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		// Initialize Spring Context
-		new ClassPathXmlApplicationContext("/META-INF/applicationContext.xml" );
-		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/applicationContext.xml" );
+		for (String beenName : context.getBeanDefinitionNames()) {
+			System.out.println("beenName="+beenName);
+		}
 		// Instantiate class by new'ing it up. i.e., Do not obtain from Spring context
 		HelloWorldClient client = new HelloWorldClient();
 		client.sayHello();
